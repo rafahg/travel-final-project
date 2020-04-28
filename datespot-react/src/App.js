dd7ca9977c7b9e3b35de4a005d3d9206056ba4aa
nimzyow <n_soufiani@hotmail.com>
2020-04-28 11:35:42 +0100

    bootstrap initialised

---
+++ datespot-react/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import { Navbar, Nav } from "react-bootstrap";
+
+function App() {
+  return <div className="App">Hello world!</div>;
+}
+
+export default App;
