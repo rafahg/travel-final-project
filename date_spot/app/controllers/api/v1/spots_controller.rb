90b519c53a54cb18fbbe3ba4204068c401e22149
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-04-28 14:38:19 +0100

    api implented and spot table done

---
+++ date_spot/app/controllers/api/v1/spots_controller.rb
@@ -0,0 +1,65 @@
+class Api::V1::SpotsController < ApplicationController
+  before_action :set_spot, only: [:show, :update, :destroy]
+
+  # GET /spots
+  # GET /spots.json
+  def index
+    @spots = Spot.all
+
+    render json: @spots
+  end
+
+  # GET /spots/1
+  # GET /spots/1.json
+  def show
+    render json: @spot
+  end
+
+  # GET /spots/new
+  # def new
+  #   @spot = Spot.new
+  # end
+
+  # GET /spots/1/edit
+  # def edit
+  # end
+
+  # POST /spots
+  # POST /spots.json
+  def create
+    @spot = Spot.new(spot_params)
+
+    if @spot.save
+     render json: @spot, status: :created, location:        api_v1_article_url(@spot)
+    else
+     render json: @spot.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /spots/1
+  # PATCH/PUT /spots/1.json
+  def update
+    if @spot.update(spot_params)
+      render json: @spot
+     else
+      render json: @spot.errors, status: :unprocessable_entity
+     end
+  end
+
+  # DELETE /spots/1
+  # DELETE /spots/1.json
+  def destroy
+    @spot.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_spot
+      @spot = Spot.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def spot_params
+      params.require(:spot).permit(:title, :description, :location, :url, :average_cost)
+    end
+end
--- date_spot/db/seeds.rb
+++ date_spot/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spot.create(title: "sunny place",description: "super sunny", location: "london", url: "www.google.com", average_cost: "3")
