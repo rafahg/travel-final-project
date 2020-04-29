607bdeafa5223fb58335959af0ad5aed8a21c1cd
sgneha <sgneha05@gmail.com>
2020-04-29 15:45:19 +0100

    updation

--- date_spot/db/schema.rb
+++ date_spot/db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_29_100434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "spots", force: :cascade do |t|
     t.string "title"
     t.string "description"
