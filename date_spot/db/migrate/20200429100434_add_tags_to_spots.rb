baa4d05e11a5fa014d676df7018840a679e222be
Rafael <r.hgar78@gmail.com>
2020-04-30 16:07:18 +0200

    travis.yml updated

--- date_spot/db/migrate/20200429100434_add_tags_to_spots.rb
+++
@@ -1,8 +0,0 @@
-class AddTagsToSpots < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tags_spots, id: false do |t|
-      t.belongs_to :tag
-      t.belongs_to :spot
-    end
-  end
-end
