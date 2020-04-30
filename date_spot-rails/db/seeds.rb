90258771ade194b4250df2d5592bdd3cf0f7c88b
Rafael <r.hgar78@gmail.com>
2020-05-01 01:14:43 +0200

    travis.yml updated

--- date_spot-rails/db/seeds.rb
+++ date_spot-rails/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Spot.create(title: "romantic evening in my house",description: "a lovely walk down little venice", location: "little venice, london", url: "https://cdn.londonandpartners.com/asset/819f66aac606c1ef23220825a910f63c.jpg", avg_cost: "3", latitude: "51.5245129", longitude: "-0.1866869")
-# Tag.create(tag: "romantic")
+Spot.create(title: "romantic evening in my house",description: "a lovely walk down little venice", location: "little venice, london", url: "https://cdn.londonandpartners.com/asset/819f66aac606c1ef23220825a910f63c.jpg", avg_cost: "3", latitude: "51.5245129", longitude: "-0.1866869")
+Tag.create(tag: "romantic")
 Spot.create(title: "Lovely pizzeria",description: "a lovely walk down little venice", location: "Barbican, london", url: "https://cdn.londonandpartners.com/asset/819f66aac606c1ef23220825a910f63c.jpg", avg_cost: "3", latitude: "51.5245129", longitude: "-0.1866869")
 Tag.create(tag: "Excellent food")
