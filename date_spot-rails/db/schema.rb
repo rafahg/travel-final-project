fc42217235e785b29c6d5b94ee2e1bddd89f0094
sgneha <sgneha05@gmail.com>
2020-04-30 10:57:26 +0100

    added tags controller

--- date_spot-rails/db/schema.rb
+++ date_spot-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_093258) do
+ActiveRecord::Schema.define(version: 2020_04_30_094629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2020_04_30_093258) do
     t.string "location"
     t.string "url"
     t.string "avg_cost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
