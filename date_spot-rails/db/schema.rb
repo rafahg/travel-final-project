17525fb4da98f9668b0e114f0d7b37cc65e2e552
sgneha <sgneha05@gmail.com>
2020-05-02 12:48:01 +0100

    implemented devise,added welcome controller,moved likes & comments controller into api/v1

--- date_spot-rails/db/schema.rb
+++ date_spot-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_101046) do
+ActiveRecord::Schema.define(version: 2020_05_02_095559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,20 @@ ActiveRecord::Schema.define(version: 2020_05_01_101046) do
     t.bigint "spot_id"
     t.index ["spot_id"], name: "index_tags_spots_on_spot_id"
     t.index ["tag_id"], name: "index_tags_spots_on_tag_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "spots"
