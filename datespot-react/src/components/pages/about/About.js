64dceb20d6b42ff4ab0c9239eb6c12ae10b28098
nimzyow <n_soufiani@hotmail.com>
2020-05-04 22:11:14 +0100

    done about page

--- datespot-react/src/components/pages/about/About.js
+++ datespot-react/src/components/pages/about/About.js
@@ -1,9 +1,72 @@
 import React from "react";
+import { Tabs, Tab, Image } from "react-bootstrap";
+import RafaImage from "../../../assets/images/rafa.png";
+import GarethImage from "../../../assets/images/gareth.png";
+import NimaImage from "../../../assets/images/nima.png";
+import Neha from "../../../assets/images/neha.png";
+import Artemis from "../../../assets/images/artemis.png";
+import Individual from "./Individual";
+
+import * as DESCRIPTION from "./Description";
 
 const About = () => {
   return (
     <div data-test="about-container">
-      <h1 style={{ color: "black" }}>This is the about page</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "10px",
+          marginTop: "50px",
+        }}
+      >
+        <h3 style={{ width: "80%" }} className="text-center">
+          Below are the people who worked on this wonderful application. Click
+          on any of their names below to find out more about them.{" "}
+        </h3>
+      </div>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="rafa" title="Rafa Hernandez">
+          <Individual
+            name="Rafa Hernandez"
+            img={RafaImage}
+            desc={DESCRIPTION.RAFA}
+            link="https://github.com/rafahg"
+          />
+        </Tab>
+        <Tab eventKey="artemis" title="Artemis Papanikolaou">
+          <Individual
+            name="Artemis Papanikolaou"
+            img={Artemis}
+            desc={DESCRIPTION.RAFA}
+            link="https://github.com/artemis-p"
+          />
+        </Tab>
+        <Tab eventKey="neha" title="Neha Singh">
+          <Individual
+            name="Neha Singh"
+            img={Neha}
+            desc={DESCRIPTION.RAFA}
+            link="https://github.com/sgneha"
+          />
+        </Tab>
+        <Tab eventKey="gareth" title="Gareth Harris">
+          <Individual
+            name="Gareth Harris"
+            img={GarethImage}
+            desc={DESCRIPTION.RAFA}
+            link="https://github.com/gdharris1000"
+          />
+        </Tab>
+        <Tab eventKey="nima" title="Nima Soufiani">
+          <Individual
+            name="Nima Soufiani"
+            img={NimaImage}
+            desc={DESCRIPTION.NIMA}
+            link="https://github.com/Nimzyow"
+          />
+        </Tab>
+      </Tabs>
     </div>
   );
 };
---
+++ datespot-react/src/components/pages/about/Individual.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+
+export const Individual = ({ img, desc, name, link }) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <Image src={img} rounded />
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <h5>{desc}</h5>
+        <h6>
+          Feel free to checkout my <a href={link}>GitHub</a> at any time!
+        </h6>
+      </div>
+    </div>
+  );
+};
+
+export default Individual;
