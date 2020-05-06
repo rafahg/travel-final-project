d807dca9a8cef775d5f5eeedbc537bef5aa520bf
nimzyow <n_soufiani@hotmail.com>
2020-05-06 09:32:42 +0100

    refactoring

---
+++ datespot-react/src/components/spot/CommentsHeader.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+export const CommentsHeader = () => {
+  return (
+    <Fragment>
+      <div style={{ borderTop: "1px solid black", marginTop: "30px" }}></div>
+      <div
+        style={{ marginTop: "5px", marginBottom: "5px" }}
+        className="text-center"
+      >
+        <h3>Feedback on this spot</h3>
+      </div>
+    </Fragment>
+  );
+};
+
+export default CommentsHeader;
